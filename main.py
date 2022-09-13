8e98e144d8e0e49656c44c6fdfe947740b549cb0
Vladislav <76568831+Vladuslov@users.noreply.github.com>
2022-09-13 20:59:17 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# 13.09.2022
+
+hight = int(input)
+if hight < 171:
+  print("в танкисты!!!")
+elif hight < 181:
+  print("в пехоту!!!")
+elif hight < 191:
+  print("в десант!!!")
+else:
+  print("привет армия!!!")
+  
